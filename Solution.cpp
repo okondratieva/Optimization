34c2caf3a01850a75820473cfdc484b0b3ce5825
okondratieva <olga-kondratieva@inbox.ru>
2018-12-11 00:07:09 +0300

    Used 4 buffers in cycle

--- Solution.cpp
+++ Solution.cpp
@@ -14,13 +14,21 @@ long my_vector_len(my_vector *v) {
 double foo_bar(my_vector *v)
 {
 	long i, len = my_vector_len(v);
-
-	double tmp = 1, tmp2 = 1;
-	for (i = 0; i < lim; i += 2) {
+	double tmp = 1, tmp2 = 1, tmp4 = 1, tmp3 = 1;
+	long lim = len - 3;
+	
+	for (i = 0; i < lim; i += 4) {
 		tmp *= v->data[i];
 		tmp2 *= v->data[i + 1];
+		tmp3 *= v->data[i + 2];
+		tmp4 *= v->data[i + 3];
 	}
-	return tmp * tmp2;
+	while (i < len)
+	{
+		tmp *= v->data[i];
+		i++;
+	}
+	return tmp*tmp2*tmp3*tmp4;
 }
 
 double a[LENGTH];
